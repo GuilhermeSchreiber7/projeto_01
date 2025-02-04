6364ddc2413ff2be5265e62fa8aa013226d58274
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2025-02-04 14:59:24 -0300

    aula PI Backend

---
+++ backend/alunos/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Estados
+
+admin.site.register(Estados)
+
+
+
---
+++ backend/alunos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Estados(models.Model):
+    nome = models.CharField(max_length=50)
+    sigla = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.nome} ({self.sigla})"

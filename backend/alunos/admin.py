ce0ef0739e7db4b1f5d6f440c2dc2b23036e0c49
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2025-02-04 15:16:51 -0300

    pessoa tabela

--- backend/alunos/admin.py
+++ backend/alunos/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Estados
+from .models import Cidade
+from .models import pessoa
 
 admin.site.register(Estados)
-
+admin.site.register(Cidade)
+admin.site.register(pessoa)
 
 
--- backend/alunos/models.py
+++ backend/alunos/models.py
@@ -6,3 +6,21 @@ class Estados(models.Model):
 
     def __str__(self):
         return f"{self.nome} ({self.sigla})"
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=100)
+    estado = models.ForeignKey(Estados, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.nome} - {self.estado.sigla}"
+
+
+class pessoa (models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.nome} ({self.email}) - {self.cidade.nome} - {self.data_nascimento}"
